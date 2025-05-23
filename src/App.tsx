e5d3e8630a8af40714c4cafaca8b3d3ce558b79a
tj-ftm <159525990+tj-ftm@users.noreply.github.com>
2025-05-23 23:01:31 +0530

    Add theme context and update color scheme

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ import Footer from './components/Footer';
 
 // Pages
 import HomePage from './pages/HomePage';
-import DeckBuilder from './pages/DeckBuilder';
 import BattleArena from './pages/BattleArena';
 import Marketplace from './pages/Marketplace';
 import ShillDashboard from './pages/ShillDashboard';
@@ -18,41 +17,43 @@ import Profile from './pages/Profile';
 // Context
 import { WalletProvider } from './context/WalletContext';
 import { GameDataProvider } from './context/GameDataContext';
+import { ThemeProvider } from './context/ThemeContext';
 
 function App() {
   return (
-    <WalletProvider>
-      <GameDataProvider>
-        <Router>
-          <div className="flex flex-col min-h-screen bg-gray-900 text-white">
-            <Navbar />
-            <main className="flex-grow">
-              <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/deck-builder" element={<DeckBuilder />} />
-                <Route path="/battle-arena" element={<BattleArena />} />
-                <Route path="/marketplace" element={<Marketplace />} />
-                <Route path="/shill-dashboard" element={<ShillDashboard />} />
-                <Route path="/profile" element={<Profile />} />
-              </Routes>
-            </main>
-            <Footer />
-          </div>
-          <ToastContainer
-            position="bottom-right"
-            autoClose={5000}
-            hideProgressBar={false}
-            newestOnTop
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="dark"
-          />
-        </Router>
-      </GameDataProvider>
-    </WalletProvider>
+    <ThemeProvider>
+      <WalletProvider>
+        <GameDataProvider>
+          <Router>
+            <div className="flex flex-col min-h-screen bg-primary-light dark:bg-primary-dark text-text-light dark:text-text-dark">
+              <Navbar />
+              <main className="flex-grow">
+                <Routes>
+                  <Route path="/" element={<HomePage />} />
+                  <Route path="/battle-arena" element={<BattleArena />} />
+                  <Route path="/marketplace" element={<Marketplace />} />
+                  <Route path="/shill-dashboard" element={<ShillDashboard />} />
+                  <Route path="/profile" element={<Profile />} />
+                </Routes>
+              </main>
+              <Footer />
+            </div>
+            <ToastContainer
+              position="bottom-right"
+              autoClose={5000}
+              hideProgressBar={false}
+              newestOnTop
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              theme="dark"
+            />
+          </Router>
+        </GameDataProvider>
+      </WalletProvider>
+    </ThemeProvider>
   );
 }
 
